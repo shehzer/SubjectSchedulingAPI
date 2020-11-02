7015748fe21955a54c7c322a73da9949a4ec04a0
Shehzer <snaumani@uwo.ca>
2020-11-01 20:29:31 -0500

    Created new put request for new schedule

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const { SSL_OP_EPHEMERAL_RSA } = require('constants');
+//const { SSL_OP_EPHEMERAL_RSA } = require('constants');
 const express = require('express');
 const app = express();
 const router = express.Router();
@@ -15,10 +15,7 @@ app.use(cors());
 
 
 
-// fs.readFile('./data.json', 'utf8', function(err,data){
-//     if(err) throw err;
-//     data = JSON.parse(data);
-// });
+
 //Setup serving front-end code
 app.use('/', express.static('static'));
 
@@ -119,132 +116,23 @@ router.route('/')
     })
 
 
-
-
-//get details for the list
-        // router.get('/', (req,res) => {
-
-            
-        // const node= data.map(function(d){
-        //         var info = {"subject": d.subject,
-        //                     "className": d.className
-        //                 }
-        //     return info;
-        //     });
-
-        //     res.send(node);
-            
-        //     });
-//get details for subject and course code
-        // router.get('/:data_subject', (req,res) => {
-        //     const subject = req.params.data_subject;
-        //     var exists = data.find(d => d.subject === subject);
-        //     var node = data.filter(function(d){
-        //         return d.subject ===subject;
-        //     })
-        //     .map(function(d){
-        //         var info = {"subject": d.subject,
-        //                     "catalog_nbr": d.catalog_nbr
-        //                 }
-        //     return info;
-        //     });
-        //     if(exists){
-        //         res.send(node);
-        //     }
-        //     else{
-        //         res.status(404).send("Subject " + subject + " was not found");
-        //     }
-            
-            
-        //     })
-    
-    
-
-
-//Task 3: But ask what is optional course
-            // router.get('/:data_subject/:data_id', (req,res) => {
-            //     const subject = req.params.data_subject;
-            //     const id = req.params.data_id;
-            //     var exists = data.find(d => d.subject === subject);
-            //     var not_Id = data.find( d=> d.catalog_nbr === id);
-            //     var node = data.filter(function(d){
-            //         return  d.subject ===subject;
-            //     })
-            //     .filter(function(d){
-            //         return d.catalog_nbr.toString() ===id;
-            //     })
-            //     .map(function(d){
-            //         var info = {
-
-            //                     "subject": d.subject,
-            //                     "catalog_nbr": d.catalog_nbr,
-            //                     "course info": d.course_info 
-            //                 }
-            //         return info;
-            //     });
-
-            //     if(!exists){
-            //         res.status(404).send("Subject " + subject + " was not found");
-            //     }
-            //     else if(!not_Id){
-            //         res.status(404).send("Class code " + id + " was not found");
-            //     }
-            //     else{
-            //         res.send(node);
-            //     }
-            // })
-
-
-
-//******************Put request does not overide  */
-//create or replace class data for a given id
-
-router.get('/schedules', (req,res) => {
-    const data = db.get("schedules").value()
-    return res.json(data);
-})
-
-router.put('/schedules/:name', (req, res) =>{
-    var name = req.params.name;
-
-    for(var i =0; i<db.getState().schedules[i].length;i++){
-        if(db.getState().schedules[i].name == name){
-            res.status(404).send("ahlie")
+//task 4
+router.put('/schedule/:name', (req,res) =>{
+    const name = req.params.name;
+    for(var i=0; i<db.getState().schedules.length;i++){
+        if(db.getState().schedules[i].scheduleName===name){
+            res.status(404).send("The schedule " + name + "has been created");
             return;
         }
-        
     }
-    db.get('schedules').push({name}).write();
-    res.json({ success: true})
-    // res.status(200).send();
-})
-
-// //Update existing schedule item
-// router.put('/:id', (req,res) => {
-//     const new_node = req.body;
-//     console.log("class : ", new_node);
-
-//     //find the node
-//     const node = data.findIndex(d => d.catalog_nbr === parseInt(req.params.id));
-
-//     if(node <0){
-//         res.status(404).send("Part" + req.params.id + "Not found");
-//     }
-//     else{
-//         console.log("Changing node for", req.params.id);
-//         data[node].stock += parseInt(req.body.stock);
-//         res.send(req.body);
-//     }
-// })
+    db.get('schedule').push({scheduleName:name,
+                                  subject:"  ",
+                                  courseName:"  "}).write();
+    res.status(200).send();
+}); 
 
 
-// //create a new schedule
-// router.post('/:schedule_name', (req,res) =>{
-//     const new_node = req.body;
-// })
 
-
-//Install the router at /api/parts
 app.use('/data', router)
 
 
